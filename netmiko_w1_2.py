51dbd5007299b9630fb25c8dad8c68f0e8965e45
Tony Fermendzin <acfe@brighthouse.com>
2019-07-14 16:57:08 -0700

    add netmiko_w1_2

---
+++ netmiko_w1_2.py
@@ -0,0 +1,28 @@
+#!/home/tfermendzin/VENV/py3_venv/bin/python3
+ 
+from netmiko import ConnectHandler
+from getpass import getpass
+
+device1 = {
+	'host': 'cisco3.lasthop.io',
+	'username': 'pyclass',
+	'password': getpass(),
+	'device_type': 'cisco_ios_ssh',
+	'session_log': 'dev1_session.txt'
+}
+
+device2 = {
+	'host': 'cisco4.lasthop.io',
+	'username': 'pyclass',
+	'password': getpass(),
+	'device_type': 'cisco_ios_ssh',
+	'session_log': 'dev2_session.txt'
+}
+
+devices = [device1, device2]
+
+for device in devices:
+    print('~'*79)
+    print('Connecting to {}'.format(device))
+    net_connect = ConnectHandler(**device)
+    print(net_connect.find_prompt())
