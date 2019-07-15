f0d861796891b7e528c6d0bc09ee933a503fa77e
Tony Fermendzin <acfe@brighthouse.com>
2019-07-14 17:15:45 -0700

    commit

---
+++ netmiko_w1_3.py
@@ -0,0 +1,32 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+username = input('Enter Username: ')
+password = getpass()
+
+device1 = {
+	'host': 'cisco3.lasthop.io',
+	'username': username,
+	'password': password,
+	'device_type': 'cisco_ios_ssh',
+	'session_log': 'dev1_session.txt'
+}
+
+device2 = {
+	'host': 'cisco4.lasthop.io',
+	'username': username,
+	'password': password,
+	'device_type': 'cisco_ios_ssh',
+	'session_log': 'dev2_session.txt'
+}
+
+devices = [device1, device2]
+
+for device in devices:
+    print('~'*79)
+    print('Connecting to {}'.format(device['host']))
+    net_connect = ConnectHandler(**device)
+    output = net_connect.send_command('show version')
+    outputFileName = device['host'] + '.showver.output.txt'
+    with open(outputFileName, 'w') as write_file:
+        write_file.write(output)
